87b5c818fee7153e4666eca9f0133e63bf375c14
Densel <densel.esekon2@student.moringaschool.com>
2023-09-17 09:21:56 +0300

    Learning functions

---
+++ index.js
@@ -0,0 +1,46 @@
+// Follow along with the examples here
+/* function sayHelloTo(name) {
+    console.log(`Hello, ${name}!`);
+  }
+
+sayHelloTo("Isabel");
+sayHelloTo("Sofia");
+sayHelloTo("Brendan"); */
+
+
+ /* function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+ say("Julio", "Hello"); */
+
+ /* function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2)); */
+
+ /* function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Sofia")); */
+
+ /* function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  console.log(say("Hello", "Sofia")); */
+
+ /* function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner")); */
+
+
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+  }
+  console.log(say("Howdy", "partner"));
